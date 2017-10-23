c65c9fe09de61fe4cfcc72b9320a88a70d0a1cd1
Jayavardhan <vardhan1529@gmail.com>
2017-10-23 17:59:38 +0530

    Sample and very basic OAuth2 implementation

---
+++ SimpleWebProjects/Oauth/Models/AuthRespository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Oauth.Models
+{
+    public class AuthRespository
+    {
+        private AuthContext authContext;
+        private UserManager<IdentityUser> userManager;
+        public AuthRespository()
+        {
+            authContext = new AuthContext();
+            userManager = new UserManager<IdentityUser>( new UserStore<IdentityUser>(authContext));
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserModel userModel)
+        {
+            IdentityUser user = new IdentityUser
+            {
+                UserName = userModel.UserName
+            };
+
+            var result = await userManager.CreateAsync(user, userModel.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser user = await userManager.FindAsync(userName, password);
+
+            return user;
+        }
+    }
+}
---
+++ SimpleWebProjects/Oauth/Providers/CustomOauthServerProvider.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Oauth.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Oauth.Providers
+{
+    public class CustomOauthServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            //AuthRespository _repo = new AuthRespository();
+
+            //IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+
+            if (string.IsNullOrWhiteSpace(context.UserName) || string.IsNullOrWhiteSpace(context.Password))
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+            }
+            else
+            {
+
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("sub", context.UserName));
+                identity.AddClaim(new Claim("role", "user"));
+
+                context.Validated(identity);
+            }
+
+            return base.GrantResourceOwnerCredentials(context);
+        }
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            string clientId;
+            string clientSecret;
+            context.TryGetFormCredentials(out clientId, out clientSecret);
+
+            if (clientId == "test" && clientSecret == "test")
+            {
+                context.Validated(clientId);
+            }
+
+            return base.ValidateClientAuthentication(context);
+        }
+
+        public override Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.ClientId));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+            return Task.FromResult(0);
+        }
+    }
+}
