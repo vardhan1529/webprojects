9c79fc20f2d4596e31f28820c2e3583abc8957e2
Jayavardhan <vardhan1529@gmail.com>
2017-11-14 13:07:02 +0530

    Simple static file handling

--- SimpleWebProjects/SAM/App_Start/RouteConfig.cs
+++ SimpleWebProjects/SAM/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAM.Handlers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,18 @@ namespace SAM
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //If a extension is needed to be served, then add a handler in system.webserver handlers to process that request.
+
+            //<add path="*.jpg" name="ImageHandler" verb="*" type="SAM.Handlers.ImageHandler" resourceType="Unspecified" preCondition="integratedMode" />
+            //<add path="*.png" name="ImageHandler" verb="*" type="System.Web.Handlers.TransferRequestHandler" resourceType="Unspecified" preCondition="integratedMode" />
+
+            // A defualt request handler can be used or a custom handler like ImageHandler that is defined above can be used.
+            // And then configure a route to handle it or ignore that route if the handler configured in the web.config to take care of the request.
+
+            //routes.IgnoreRoute("{name}.jpg");
+
+            routes.Add(new Route("{name}.jpg", new ImageRouteHandler()));
 
             routes.MapMvcAttributeRoutes();
 
---
+++ SimpleWebProjects/SAM/Handlers/ImageRouteHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace SAM.Handlers
+{
+    public class ImageRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new ImageHandler();
+        }
+    }
+}
